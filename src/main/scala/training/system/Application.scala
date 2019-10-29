8e9ec1010affac45e44537d35d95351282ad8db7
Nikita Lukin <nikitadecember@mail.ru>
2019-10-29 18:03:12 +0300

    added time check function

--- src/main/scala/training/system/Application.scala
+++ src/main/scala/training/system/Application.scala
@@ -45,17 +45,48 @@ object Application extends App {
       false
   }
 
+  def timeCheck1(time: String, activity_type: String): Integer = {
+    if (activity_type.equals("V") && (time.slice(8, 10).toInt >= 6 && time.slice(8, 10).toInt < 11))
+      1
+    else if (activity_type.equals("V") && (time.slice(8, 10).toInt >= 11 && time.slice(8, 10).toInt < 19))
+      2
+    else if (activity_type.equals("V") && (time.slice(8, 10).toInt >= 19 && time.slice(8, 10).toInt < 23))
+      3
+    else if (activity_type.equals("V"))
+      4
+    else if (activity_type.equals("S") && (time.slice(8, 10).toInt >= 6 && time.slice(8, 10).toInt < 11))
+      5
+    else if (activity_type.equals("S") && (time.slice(8, 10).toInt >= 11 && time.slice(8, 10).toInt < 19))
+      6
+    else if (activity_type.equals("S") && (time.slice(8, 10).toInt >= 19 && time.slice(8, 10).toInt < 23))
+      7
+    else if (activity_type.equals("S"))
+      8
+    else -1
+  }
+
   val first_stage = sc
     .textFile("./dataset/testLeague/1st_stage/*")
-    .map(line => line.split("\\|", -1)).map(array => (array(1), array(2), array(32),0,0,0,0,0,0,0,0))
-
-  .coalesce(1).saveAsTextFile("./output/test1")
-
-/*
-  val file = new File("./output/test_output")
-  val bw = new BufferedWriter(new FileWriter(file))
-  bw.write(first_stage.mkString("\n"))
-  bw.close()*/
+    .map(line => line.split("\\|", -1))
+    .map(array => (array(1), array(2), array(32)))
+    .map {
+      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 1 => ((phone, date.substring(0, 8)), (1, 0, 0, 0, 0, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 2 => ((phone, date.substring(0, 8)), (0, 1, 0, 0, 0, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 3 => ((phone, date.substring(0, 8)), (0, 0, 1, 0, 0, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 4 => ((phone, date.substring(0, 8)), (0, 0, 0, 1, 0, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 5 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 1, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 6 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 0, 1, 0, 0))
+      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 7 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 0, 0, 1, 0))
+      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 8 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 0, 0, 0, 1))
+      case (phone, date, activity_type) if timeCheck1(date, activity_type) == -1 => None
+    }
+    //.coalesce(1).saveAsTextFile("./output/test1")
+first_stage
+  /*
+    val file = new File("./output/test_output")
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(first_stage.mkString("\n"))
+    bw.close()*/
 
   sc.stop()
 }
