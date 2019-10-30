04e9e6552986a5e286cb172a51eecddf1c9af912
Nikita Lukin <nikitadecember@mail.ru>
2019-10-30 09:43:46 +0300

    second and third stages done

--- src/main/scala/training/system/Application.scala
+++ src/main/scala/training/system/Application.scala
@@ -1,27 +1,10 @@
 package training.system
 
-import java.io.{BufferedWriter, FileWriter}
-import java.sql.{Date, Timestamp}
+import java.io.{BufferedWriter, File, FileWriter}
 import java.text.SimpleDateFormat
 
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.FileSystem
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
-
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
-
-import scala.io.Source
-import java.io._
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
-
-import scala.util.Try
-
-import scala.util.Try
 
 
 object Application extends App {
@@ -35,17 +18,7 @@ object Application extends App {
 
   val sc = new SparkContext(sparkConf)
 
-  def timeCheck(time: String) = {
-    val format = "yyyy-MM-dd HH:mm:ss"
-    val df = new SimpleDateFormat(format)
-    val tmp = df.format(time.toLong)
-    if (((tmp.slice(11, 13).toInt - 3) >= 9) && ((tmp.slice(11, 13).toInt - 3) < 17))
-      true
-    else
-      false
-  }
-
-  def timeCheck1(time: String, activity_type: String): Integer = {
+  def timeCheck(time: String, activity_type: String): Integer = {
     if (activity_type.equals("V") && (time.slice(8, 10).toInt >= 6 && time.slice(8, 10).toInt < 11))
       1
     else if (activity_type.equals("V") && (time.slice(8, 10).toInt >= 11 && time.slice(8, 10).toInt < 19))
@@ -65,31 +38,52 @@ object Application extends App {
     else -1
   }
 
-  val first_stage = sc
+  val stage_1 = sc
     .textFile("./dataset/testLeague/1st_stage/*")
     .map(line => line.split("\\|", -1))
     .map(array => (array(1), array(2), array(32)))
     .map {
-      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 1 => ((phone, date.substring(0, 8)), (1, 0, 0, 0, 0, 0, 0, 0))
-      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 2 => ((phone, date.substring(0, 8)), (0, 1, 0, 0, 0, 0, 0, 0))
-      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 3 => ((phone, date.substring(0, 8)), (0, 0, 1, 0, 0, 0, 0, 0))
-      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 4 => ((phone, date.substring(0, 8)), (0, 0, 0, 1, 0, 0, 0, 0))
-      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 5 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 1, 0, 0, 0))
-      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 6 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 0, 1, 0, 0))
-      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 7 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 0, 0, 1, 0))
-      case (phone, date, activity_type) if timeCheck1(date, activity_type) == 8 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 0, 0, 0, 1))
-      //case (phone, date, activity_type) if timeCheck1(date, activity_type) == -1 => None
+      case (phone, date, activity_type) if timeCheck(date, activity_type) == 1 => ((phone, date.substring(0, 8)), (1, 0, 0, 0, 0, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck(date, activity_type) == 2 => ((phone, date.substring(0, 8)), (0, 1, 0, 0, 0, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck(date, activity_type) == 3 => ((phone, date.substring(0, 8)), (0, 0, 1, 0, 0, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck(date, activity_type) == 4 => ((phone, date.substring(0, 8)), (0, 0, 0, 1, 0, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck(date, activity_type) == 5 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 1, 0, 0, 0))
+      case (phone, date, activity_type) if timeCheck(date, activity_type) == 6 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 0, 1, 0, 0))
+      case (phone, date, activity_type) if timeCheck(date, activity_type) == 7 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 0, 0, 1, 0))
+      case (phone, date, activity_type) if timeCheck(date, activity_type) == 8 => ((phone, date.substring(0, 8)), (0, 0, 0, 0, 0, 0, 0, 1))
     }.reduceByKey {
     case ((v_morn_ac, v_day_ac, v_even_ac, v_night_ac, s_morn_ac, s_day_ac, s_even_ac, s_night_ac), (v_morn, v_day, v_even, v_night, s_morn, s_day, s_even, s_night))
     => (v_morn_ac + v_morn, v_day_ac + v_day, v_even_ac + v_even, v_night_ac + v_night, s_morn_ac + s_morn, s_day_ac + s_day, s_even_ac + s_even, s_night_ac + s_night)
+  }.map{
+    case ((phone, date), (v_morn, v_day, v_even, v_night, s_morn, s_day, s_even, s_night)) =>
+      (phone, (date, v_morn, v_day, v_even, v_night, s_morn, s_day, s_even, s_night))
   }
-  .coalesce(1).saveAsTextFile("./output/test1")
 
-  /*
+
+  val stage2_name = sc
+    .textFile("./dataset/testLeague/2nd_stage/ban/*")
+    .map(line => line.split("\u0001", -1))
+    .map(array => (array(0), array(14)))
+
+  val stage2_phone = sc
+    .textFile("./dataset/testLeague/2nd_stage/subs/*")
+    .map(line => line.split("\u0001", -1))
+    .map(array => (array(0), array(1)))
+
+  val stage2_joined = stage2_phone.leftOuterJoin(stage2_name).map{
+    case (ban, (phone, name)) => (phone, name.getOrElse(None))
+  }
+
+  val stage_3 = stage_1.leftOuterJoin(stage2_joined).map {
+    case (phone, ((date, v_morn, v_day, v_even, v_night, s_morn, s_day, s_even, s_night), name)) =>
+      (phone, date, v_morn, v_day, v_even, v_night, s_morn, s_day, s_even, s_night, name.getOrElse(None))
+  }.collect()
+   // .coalesce(1).saveAsTextFile("./output/test6")
+
     val file = new File("./output/test_output")
     val bw = new BufferedWriter(new FileWriter(file))
-    bw.write(first_stage.mkString("\n"))
-    bw.close()*/
+    bw.write(stage_3.mkString("\n"))
+    bw.close()
 
   sc.stop()
 }
