e12b78bd729eb8af851f678962ce37682825a628
Nikita Lukin <nikitadecember@mail.ru>
2019-10-30 16:25:39 +0300

    one more output format fix

--- src/main/scala/training/system/stages/Stage3.scala
+++ src/main/scala/training/system/stages/Stage3.scala
@@ -4,10 +4,10 @@ import org.apache.spark.rdd.RDD
 
 
 class Stage3 {
-  def load(rdd1: RDD[(String, (String, Array[Int]))], rdd2: RDD[(String, String)]): RDD[(String, Int, Int, Int, Int, Int, Int, Int, Int, String)] = {
+  def load(rdd1: RDD[(String, (String, Array[Int]))], rdd2: RDD[(String, String)]): RDD[(String, String, String)] = {
     rdd1.leftOuterJoin(rdd2).map {
-      case (phone, ((date, Array(x1, x2, x3, x4, x5, x6, x7, x8)), name)) =>
-        (phone, x1, x2, x3, x4, x5, x6, x7, x8, name.getOrElse("NO_DATA"))
+      case (phone, ((date, arr), name)) =>
+        (phone, arr.mkString(","), name.getOrElse("NO_DATA"))
     }
   }
 }
